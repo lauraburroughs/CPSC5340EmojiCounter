f5313319a905579e4da38eda3f96f52bbd6d28db
Laura Burroughs <hellolauraburroughs@gmail.com>
2026-03-21 06:19:57 -0500

    Initial Commit

---
+++ EmojiCounter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmojiCounter
+//
+//  Created by Laura Burroughs on 3/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
